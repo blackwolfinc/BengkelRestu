78389c33ab74c3e37e5981a9807e4b56af284dce
Black Wolf Team <47728533+blackwolfinc@users.noreply.github.com>
2023-04-12 23:02:41 +0700

    fix number telepone

--- src/constants/socialMediaLink.js
+++ src/constants/socialMediaLink.js
@@ -1,6 +1,6 @@
 export const SOCIAL_MEDIA_LINK = {
   FACEBOOK: "https://www.facebook.com/profile.php?id=100090688511478&mibextid=ZbWKwL",
   INSTAGRAM: "https://www.instagram.com/bengkelbubutrestu/",
-  WHATSAPP: "https://wa.me/+6285784258128",
-  WHATSAPP2: "https://wa.me/+6282145473960",
+  WHATSAPP: "https://wa.me/+6281335763070",
+  WHATSAPP2: "https://wa.me/+6281335763070",
 };
